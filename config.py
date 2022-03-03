c91d822b5392aec5ff683ff98b5443b525e031f6
Brian Otieno <93243367+Otybrian@users.noreply.github.com>
2022-03-03 03:58:15 +0300

    add configerations

---
+++ config.py
@@ -0,0 +1,26 @@
+from distutils.debug import DEBUG
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = 'i-am-the-master'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
